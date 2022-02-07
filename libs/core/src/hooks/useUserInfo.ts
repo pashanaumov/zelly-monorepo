fd462544ea0d2c8692c7ae6c0af96af9497698d6
Pavel Naumov <pavliknaumov@gmail.com>
2022-02-07 22:48:05 +0300

    feat: userinfo hook

---
+++ libs/core/src/hooks/useUserInfo.ts
@@ -0,0 +1,25 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/storeWeb';
+
+export function useUserInfo() {
+  const user = useSelector((state: RootState) => state.user.user);
+
+  if (!user) {
+    return {
+      country: '',
+      language: '',
+      id: '',
+      email: '',
+      token: '',
+    };
+  }
+
+  return {
+    country: user?.country || '',
+    language: user?.language || '',
+    id: user?.id || '',
+    email: user?.email || '',
+    token: user?.token || '',
+    username: user?.email.substring(0, user?.email.lastIndexOf('@')) || '',
+  };
+}
