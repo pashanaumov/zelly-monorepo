a3dd8cdd92def20d25a73af5856c82a0aa07f218
Pavel Naumov <pavliknaumov@gmail.com>
2022-05-17 23:36:47 +0400

    feat: ios icons

--- apps/zelly-web/server/server.js
+++ apps/zelly-web/server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '../../dist', 'zelly-web');
 
 const port = process.env.PORT || 3000;
 
