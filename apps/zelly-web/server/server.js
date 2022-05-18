1ac2e8e72ca42801a897a8cac5ede595934b0fa5
Pavel Naumov <pavliknaumov@gmail.com>
2022-05-18 09:57:15 +0400

    feat: add Procfile

--- apps/zelly-web/server/server.js
+++ apps/zelly-web/server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '../../../dist/apps', 'zelly-web');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(publicPath));
 
