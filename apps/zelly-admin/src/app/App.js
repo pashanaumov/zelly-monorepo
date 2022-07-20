c92aedee126cd88d3539016fdb84393d86f2fb14
Pavel Naumov <pavliknaumov@gmail.com>
2022-07-20 22:18:15 +0400

    feat: [admin] - init admin

---
+++ apps/zelly-admin/src/app/App.js
@@ -0,0 +1,38 @@
+import React, { Component, Suspense, lazy } from 'react'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import '../scss/style.scss'
+
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+)
+
+// Containers
+const DefaultLayout = lazy(() => import('../layout/DefaultLayout'))
+
+// Pages
+const Login = lazy(() => import('../views/pages/login/Login'))
+const Register = lazy(() => import('../views/pages/register/Register'))
+const Page404 = lazy(() => import('../views/pages/page404/Page404'))
+const Page500 = lazy(() => import('../views/pages/page500/Page500'))
+
+class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <Suspense fallback={loading}>
+          <Routes>
+            <Route exact path="/login" name="Login Page" element={<Login />} />
+            <Route exact path="/register" name="Register Page" element={<Register />} />
+            <Route exact path="/404" name="Page 404" element={<Page404 />} />
+            <Route exact path="/500" name="Page 500" element={<Page500 />} />
+            <Route path="*" name="Home" element={<DefaultLayout />} />
+          </Routes>
+        </Suspense>
+      </HashRouter>
+    )
+  }
+}
+
+export default App
